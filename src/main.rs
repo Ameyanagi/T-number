0e165a84244b329efc08063c2bb6dd6f0417e6cd
Ameyanagi <ryuichi@ameyanagi.com>
2024-03-02 02:43:02 -0500

    update readme with linebreak

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,17 @@ use std::ops::RangeBounds;
 
 use primefactor::PrimeFactors;
 
-fn is_two_prime(x: u128) -> bool {
-    let prime_factor = PrimeFactors::from(x);
-
-    if prime_factor.len() != 2 {
-        return false;
-    }
-
-    for int_factor in prime_factor.iter() {
-        if int_factor.exponent > 1 {
-            return false;
-        }
-    }
-
-    true
-}
-
-fn calc_two_prime(x: u128) -> Option<Vec<u128>> {
+fn two_prime(x: u128) -> Option<Vec<u128>> {
     let prime_factor = PrimeFactors::from(x);
 
     if prime_factor.len() != 2 {
         return None;
+    }
+
+    for int_factor in prime_factor.iter() {
+        if int_factor.exponent > 1 {
+            return None;
+        }
     }
 
     Some(
@@ -33,26 +23,11 @@ fn calc_two_prime(x: u128) -> Option<Vec<u128>> {
     )
 }
 
-fn is_three_prime(x: u128) -> bool {
+fn three_prime(x: u128) -> Option<Vec<u128>> {
     let prime_factor = PrimeFactors::from(x);
-    if prime_factor.len() != 3 {
-        return false;
-    }
-    for int_factor in prime_factor.iter() {
-        if int_factor.exponent > 1 {
-            return false;
-        }
-    }
-    true
-}
-
-fn calc_three_prime(x: u128) -> Option<Vec<u128>> {
-    let prime_factor = PrimeFactors::from(x);
-
     if prime_factor.len() != 3 {
         return None;
     }
-
     Some(
         prime_factor
             .iter()
@@ -71,6 +46,14 @@ fn revert_number(x: u128) -> u128 {
     reverted_number
 }
 
+fn is_all_different(pq: &[u128], rst: &[u128]) -> bool {
+    let mut all_numbers = pq.iter().chain(rst.iter()).collect::<Vec<_>>();
+    let length = all_numbers.len();
+    all_numbers.sort();
+    all_numbers.dedup();
+    all_numbers.len() == length
+}
+
 fn find_t_number<R>(range: R) -> Vec<u128>
 where
     R: IntoIterator<Item = u128>,
@@ -78,36 +61,30 @@ where
     let mut t_number: Vec<u128> = Vec::new();
 
     for number in range {
-        if is_two_prime(number) && is_three_prime(revert_number(number)) {
-            t_number.push(number);
+        if let Some(pq) = two_prime(number) {
+            if let Some(rst) = three_prime(revert_number(number)) {
+                if is_all_different(&pq, &rst) {
+                    t_number.push(number);
+                    println!(
+                        "{}: ({}, {}), {}: ({}, {}, {})",
+                        number,
+                        pq[0],
+                        pq[1],
+                        revert_number(number),
+                        rst[0],
+                        rst[1],
+                        rst[2]
+                    );
+                }
+            }
         }
     }
 
     t_number
 }
 
-fn print_t_number(t_number: Vec<u128>) {
-    for number in t_number {
-        let pq = calc_two_prime(number).unwrap();
-        let rst = calc_three_prime(revert_number(number)).unwrap();
-
-        println!(
-            "{}: ({}, {}), {}: ({}, {}, {})",
-            number,
-            pq[0],
-            pq[1],
-            revert_number(number),
-            rst[0],
-            rst[1],
-            rst[2]
-        );
-    }
-}
-
 fn main() {
     let t_number = find_t_number(1000..10000);
-
-    print_t_number(t_number);
 }
 
 #[cfg(test)]
